3cd30f4b3d660601b43c893861b3def3688ab42a
Krishnan Mani <km@krishnanm.com>
2016-04-23 22:34:24 +0530

    Starting to get the EC2 API spec into shape

--- spec/EC2/EC2_reserved_instances_pricing_spec.rb
+++ spec/EC2/EC2_reserved_instances_pricing_spec.rb
@@ -4,17 +4,12 @@ RSpec.describe EC2Pricing do
 
   it 'GET product families' do
 
-    get '/AmazonEC2/20151209144527/product_families'
+    get '/v1.0/AmazonEC2/20151209144527/product_families'
     expect(last_response).to be_ok
     product_families = last_response.body
 
     expected_product_families = '["Compute Instance","Data Transfer","Dedicated Host","Fee","IP Address","Load Balancer","Storage","Storage Snapshot","System Operation"]'
     expect(product_families).to eql(expected_product_families)
-  end
-
-  it 'GET reserved instance pricing' do
-    get '/AmazonEC2/20151209144527/reserved'
-    expect(last_response).to be_ok
   end
 
 end
